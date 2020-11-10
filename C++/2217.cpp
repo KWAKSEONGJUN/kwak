c9899d7e28e6254d55e2d023d5769af15bdbf323
KWAKSEONGJUN <sjman223@naver.com>
2020-11-10 21:37:29 +0800

    commit

---
+++ C++/2217.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    int N, mx=-1;
+    int w[100001];
+    scanf("%d", &N);
+
+    for(int i=0; i<N; i++)
+    {
+        scanf("%d", &w[i]);   
+    }
+
+    sort(w, w + N);
+    
+    for(int i=0; i<N; i++)
+    {
+        mx = max(mx, w[i]*(N-i));
+    }
+
+    printf("%d\n", mx);
+
+    return 0;
+
+}
