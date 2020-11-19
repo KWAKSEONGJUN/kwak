b6172fe41b7dc3601e1cd26196d8d77b644e7bbe
KWAKSEONGJUN <sjman223@naver.com>
2020-11-19 18:15:07 +0800

    "c"

--- C++/test.cpp
+++ C++/test.cpp
@@ -4,6 +4,16 @@ using namespace std;
 
 int main()
 {
-	printf("hello world\n");
+	int T, a, b;
+
+	scanf("%d", &T);
+
+	for(int i=0; i<T; i++)
+	{
+		scanf("%d %d", &a, &b);
+		printf("%d\n", a+b);
+	}
+
+	
 	return 0;
 }
